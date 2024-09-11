23722882cc5fa4332528bc8e35e8f9557f093cde
Linas Galumbauskas <linas@yardventure.io>
2024-09-11 15:57:51 +0300

    error types fixed

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,6 @@
+import { redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
 export const formatPrice = (value: string | number) => {
   const price = typeof value === 'string' ? parseInt(value) : value;
 
@@ -9,4 +12,34 @@ export const formatPrice = (value: string | number) => {
   }).format(convertToFloat);
 
   return dollarsAmount;
+};
+
+export const throwErrorWithResponse = (error: unknown, msg: string) => {
+  const errorWithResponse = error as {
+    response?: {
+      data?: {
+        error?: {
+          message?: string;
+        };
+      };
+      status?: number;
+    };
+  };
+
+  let errorMessage = msg;
+
+  if (errorWithResponse && 'response' in errorWithResponse) {
+    errorMessage = errorWithResponse.response?.data?.error?.message || msg;
+  }
+
+  toast.error(errorMessage);
+
+  if (
+    errorWithResponse.response?.status === 401 ||
+    errorWithResponse.response?.status === 403
+  ) {
+    return redirect('/login');
+  }
+
+  return null;
 };
